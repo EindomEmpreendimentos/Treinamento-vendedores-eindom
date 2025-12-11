9369a6c625c5a0a119b44ce0568d3bd36ec63e57
Gabriel-Ferraz <gabrielfcontato2@gmail.com>
2025-12-11 15:52:52 -0300

    adiciona nova url

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = import.meta.env.VITE_API_URL || 'https://mis-backend-h3hdc4gdbbfdbdeb.brazilsouth-01.azurewebsites.net';
 
 export const api = axios.create({ baseURL });
 
